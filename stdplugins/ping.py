6aa5affe299cf1dcfe9d8f40e9cd3380df0b391c
Kakarotto22 <57814819+Kakarotto22@users.noreply.github.com>
2019-11-27 18:20:25 +0200

    تغيير الأمر من أجل الحصول على البينغ

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="porn ?(.*)", allow_sudo=True))
+@borg.on(admin_cmd(pattern="بينغ ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
