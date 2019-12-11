0a7f807e12a2e9c66da1164e6e96f8233f85fa84
Kakarotto22 <57814819+Kakarotto22@users.noreply.github.com>
2019-12-11 21:47:35 +0200

    Update time.py

--- stdplugins/time.py
+++ stdplugins/time.py
@@ -1,5 +1,5 @@
 """ It does not do to dwell on dreams and forget to live
-Syntax: .time"""
+Syntax: .وقت"""
 
 import asyncio
 import os
@@ -11,11 +11,11 @@ from uniborg.util import admin_cmd
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
 
-@borg.on(admin_cmd("time ?(.*)"))  # pylint:disable=E0602
+@borg.on(admin_cmd("وقت ?(.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n⚡PØRNHUB TIMEZØNE⚡ \n LOCATION: INDIA \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+    current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n⚡Noula⚡ \n LOCATION: SYRIA \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
     start = datetime.now()
     input_str = event.pattern_match.group(1)
     reply_msg_id = event.message.id
@@ -36,7 +36,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @R4V4N4",
+        caption="يعمل بواسطة : @channel22_22",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id
     )
